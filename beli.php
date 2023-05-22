6a7803172f6e21af1e507ca210f61139917837d1
SendiJulpani <130447479+SendiJulpani@users.noreply.github.com>
2023-05-22 08:27:27 +0700

    beli.php

--- beli.php
+++ beli.php
@@ -13,7 +13,7 @@ else
 	$_SESSION['pesanan'][$id_menu]=1;
 }
 
-echo "<script>alert('Produk telah masuk ke pesanan anda');</script>";
+echo "<script>alert('Produk telah masuk ke keranjang pesanan anda');</script>";
 echo "<script>location= 'pesanan_pembeli.php'</script>";
 
  ?>
