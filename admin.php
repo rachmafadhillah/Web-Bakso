038ed98f64c00617bdb666d37b462a203eeabdd7
Kevin Satria D <130447401+kevinnsd1@users.noreply.github.com>
2023-05-23 14:50:05 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@
   <!-- Navbar -->
       <nav class="navbar navbar-expand-lg  bg-dark">
         <div class="container">
-        <a class="navbar-brand text-white" href="admin.php"><strong>Bakso</strong> Abrag</a>
+        <a class="navbar-brand text-white" href="admin.php"><strong>BAKSO</strong> ABRAG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
